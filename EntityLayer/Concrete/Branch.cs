f651a9cdcfb6f8210c3c4252d85df38529625cba
ALPER BUĞRA POLAT <202802052@ogr.cbu.edu.tr>
2022-12-19 15:21:03 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Branch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Branch
+    {
+        [Key]
+        public int BranchID { get; set; }
+        [StringLength(50)]
+        public string BranchName { get; set; }
+
+
+        public int SecretaryID { get; set; }
+        public virtual Secretary Secretary { get; set; }
+
+        public ICollection<Doctor> Doctors { get; set; }
+
+    }
+}
---
+++ RandevuOtomasyon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RandevuOtomasyon.MvcApplication" Language="C#" %>
